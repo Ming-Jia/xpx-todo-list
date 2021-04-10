fe22778482d179c64b6e4d33f630c8a7b35d9e71
Yong Ming-Jia <yongmingjia0112@gmail.com>
2021-04-10 10:59:30 +0800

    Add a server folder to handling all the blockchain logic

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.listen(5000, () => {
+  console.log('Server port number: 5000');
+});
